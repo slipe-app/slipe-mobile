54ba4581bc995af28130ca2793858ddab68d392c
Dikiy Perec <hopabsq@mail.ru>
2024-08-30 21:34:18 +0600

    Last commit ig

--- components/common/navigation/navigation.jsx
+++ components/common/navigation/navigation.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useRef } from "react";
 import { View, Animated } from "react-native";
 import { BlurView } from "expo-blur";
-import styles from "./styles";
+import Fireit from "../icons/fireit.svg"
 import { Link, usePathname } from "expo-router";
+import Svg, { Path } from "react-native-svg";
+import styles from "./styles";
 import colors from "../../../constants/colors";
 import transitions from "../../../constants/transitions";
 
-export default function TabBar() {
+const TabBar = () => {
   const pathname = usePathname();
 
+  // Создаем анимированные значения и сразу запускаем анимацию при изменении pathname
   const colorAnim1 = useRef(new Animated.Value(0)).current;
   const colorAnim2 = useRef(new Animated.Value(0)).current;
   const colorAnim3 = useRef(new Animated.Value(0)).current;
 
+  // Анимация изменения цвета
   const animateColor = (colorAnim, isActive) => {
     Animated.timing(colorAnim, {
       toValue: isActive ? 1 : 0,
@@ -21,44 +25,99 @@ export default function TabBar() {
     }).start();
   };
 
+  // Обновляем анимации при изменении pathname
   useEffect(() => {
     animateColor(colorAnim1, pathname === "/");
     animateColor(colorAnim2, pathname === "/profile");
     animateColor(colorAnim3, pathname === "/search");
   }, [pathname]);
 
-  const textColor1 = colorAnim1.interpolate({
-    inputRange: [0, 1],
-    outputRange: [colors.textPrimaryTransparent, colors.text],
-  });
-
-  const textColor2 = colorAnim2.interpolate({
-    inputRange: [0, 1],
-    outputRange: [colors.textPrimaryTransparent, colors.text],
-  });
-
-  const textColor3 = colorAnim3.interpolate({
-    inputRange: [0, 1],
-    outputRange: [colors.textPrimaryTransparent, colors.text],
-  });
+  // Интерполяция значений для цвета
+  const interpolateUtil = (condition, from, to) => {
+    return condition.interpolate({
+      inputRange: [0, 1],
+      outputRange: [from, to],
+    });
+  };
 
   return (
-    <BlurView intensity={50} experimentalBlurMethod="dimezisBlurView" style={styles.tabBar} tint="systemChromeMaterialDark">
+    <BlurView
+      intensity={50}
+      experimentalBlurMethod="dimezisBlurView"
+      style={styles.tabBar}
+      tint="systemChromeMaterialDark"
+    >
       <Link style={styles.tabbarButton} href="/">
-        <Animated.Text style={{ color: textColor1 }}>
-          Blogs
-        </Animated.Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+         <Fireit style={{ color: interpolateUtil(
+                colorAnim2,
+                colors.textPrimaryTransparent,
+                colors.text
+              )}} width="60" height="60" />
+          <Animated.Text
+            style={[
+              {
+                color: interpolateUtil(
+                  colorAnim1,
+                  colors.textPrimaryTransparent,
+                  colors.text
+                ),
+              },
+              { marginLeft: 8 },
+            ]}
+          >
+            Blogs
+          </Animated.Text>
+        </View>
       </Link>
       <Link style={styles.tabbarButton} href="/profile">
-        <Animated.Text style={{ color: textColor2 }}>
-          Create post
-        </Animated.Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Fireit style={{ color: interpolateUtil(
+                colorAnim2,
+                colors.textPrimaryTransparent,
+                colors.text
+              )}} width="60" height="60" />
+          <Animated.Text
+            style={[
+              {
+                color: interpolateUtil(
+                  colorAnim2,
+                  colors.textPrimaryTransparent,
+                  colors.text
+                ),
+              },
+              { marginLeft: 8 },
+            ]}
+          >
+            Create post
+          </Animated.Text>
+        </View>
       </Link>
       <Link style={styles.tabbarButton} href="/search">
-        <Animated.Text style={{ color: textColor3 }}>
-          My profile
-        </Animated.Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Fireit style={{ color: interpolateUtil(
+                colorAnim2,
+                colors.textPrimaryTransparent,
+                colors.text
+              )}} width="60" height="60" />
+          <Animated.Text
+            style={[
+              {
+                color: interpolateUtil(
+                  colorAnim3,
+                  colors.textPrimaryTransparent,
+                  colors.text
+                ),
+              },
+              { marginLeft: 8 },
+            ]}
+          >
+            My profile
+          </Animated.Text>
+        </View>
       </Link>
     </BlurView>
   );
-}
+};
+
+export default TabBar;
